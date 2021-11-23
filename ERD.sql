9482fd4164ca39dbedb229354d8dbadd7b6b519c
nedamzd <87897023+nedamzd@users.noreply.github.com>
2021-11-23 18:25:10 -0500

    Add files via upload

---
+++ ERD.sql
@@ -0,0 +1,65 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/Mitut6
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "country" (
+    "iso_code" VARCHAR   NOT NULL,
+    "country_name" VARCHAR   NOT NULL,
+    "continent" VARCHAR   NOT NULL,
+    "population" INT,
+    "population_density" INT,
+    "gdp_per_capita" INT,
+    "extreme_poverty" INT,
+    "life_expectancy" INT,
+    "handwashing_facilities" INT,
+    "hospital_beds_per_thousand" INT,
+    CONSTRAINT "pk_country" PRIMARY KEY (
+        "iso_code"
+     )
+);
+
+CREATE TABLE "cases" (
+    "iso_code" VARCHAR   NOT NULL,
+    "date" VARCHAR   NOT NULL,
+    "total_cases_per_million" INT,
+    "new_cases_per_million" INT,
+    "new_cases_smoothed_per_million" INT
+);
+
+CREATE TABLE "deaths" (
+    "iso_code" VARCHAR   NOT NULL,
+    "date" VARCHAR   NOT NULL,
+    "total_deaths_per_million" INT,
+    "new_deaths_per_million" INT,
+    "new_deaths_smoothed_per_million" INT,
+    "excess_mortality_cumulative_per_million" INT
+);
+
+CREATE TABLE "vaccines" (
+    "iso_code" VARCHAR   NOT NULL,
+    "date" VARCHAR   NOT NULL,
+    "people_vaccinated_per_hundred" INT,
+    "people_fully_vaccinated_per_hundred" INT,
+    "total_boosters_per_hundred" INT,
+    "new_vaccinations_smoothed_per_million" INT
+);
+
+CREATE TABLE "policy" (
+    "iso_code" VARCHAR   NOT NULL,
+    "date" VARCHAR   NOT NULL,
+    "stringency_index" INT
+);
+
+ALTER TABLE "cases" ADD CONSTRAINT "fk_cases_iso_code" FOREIGN KEY("iso_code")
+REFERENCES "country" ("iso_code");
+
+ALTER TABLE "deaths" ADD CONSTRAINT "fk_deaths_iso_code" FOREIGN KEY("iso_code")
+REFERENCES "country" ("iso_code");
+
+ALTER TABLE "vaccines" ADD CONSTRAINT "fk_vaccines_iso_code" FOREIGN KEY("iso_code")
+REFERENCES "country" ("iso_code");
+
+ALTER TABLE "policy" ADD CONSTRAINT "fk_policy_iso_code" FOREIGN KEY("iso_code")
+REFERENCES "country" ("iso_code");
+
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Project-3-worldwide-vaccination
+# worldwide-vaccination
+
+
+In this dashboard we hope to present trends for the 2020-2021 data regarding COVID vaccinations, cases, deaths, and much more. Our rationale for this dashboard was to make relevant the circumstances from country to country and to allow the viewer to educate themselves on the effects of COVID-19 worldwide. Our dashboard would be useful for travelers or those interested in researching the COVID-19 pandemic and its trends. 
